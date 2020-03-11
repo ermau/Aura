27708452fa9532971be846d14ed0e5aecd2afe09
Eric Maupin <me@ermau.com>
2020-03-10 20:39:54 -0400

    Create campaign dialog

---
+++ Aura/CreateCampaignDialog.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Aura
+{
+	public sealed partial class CreateCampaignDialog
+		: ContentDialog
+	{
+		public CreateCampaignDialog ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
