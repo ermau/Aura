eb1bfeb722f3ab7c65e1276139d34546ed885fc4
Eric Maupin <me@ermau.com>
2020-05-07 22:55:58 -0400

    Start services

---
+++ Aura.Core/IDiscoverableService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aura
+{
+	public interface IDiscoverableService
+		: IService
+	{
+		/// <summary>
+		/// Attempts to discover the presence of a service, allowing Aura to prompt the user to enable it.
+		/// </summary>
+		/// <returns><c>true</c> if the service was found. <c>false</c> is not definitive.</returns>
+		Task<bool> DiscoverAsync ();
+	}
+}
---
+++ Aura.Core/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura
+{
+	public interface IService
+	{
+		/// <summary>
+		/// Gets a localized display name for the service.
+		/// </summary>
+		/// <remarks>This is used in settings and prompts to enable the service.</remarks>
+		string DisplayName { get; }
+	}
+}
