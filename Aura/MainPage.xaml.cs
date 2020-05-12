9f2e257e7ebc83b894c2d957fe2410c4ced873de
Eric Maupin <me@ermau.com>
2020-05-12 18:41:36 -0400

    Add page stubs

--- Aura/MainPage.xaml.cs
+++ Aura/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 namespace Aura
 {
@@ -8,5 +13,33 @@ namespace Aura
         {
             InitializeComponent();
         }
+
+        private void OnNavigationSelectionChanged (NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+                this.contentFrame.Navigate (typeof (SettingsPage));
+            else {
+                this.contentFrame.Navigate (PageMap[(string)sender.Tag]);
+            }
+        }
+
+        private void OnHomeTapped (object sender, TappedRoutedEventArgs e)
+        {
+            ((FrameworkElement)sender).ContextFlyout.ShowAt ((FrameworkElement)sender);
+        }
+
+        private void OnCampaignTapped (object sender, TappedRoutedEventArgs e)
+        {
+            ((FrameworkElement)sender).ContextFlyout.ShowAt ((FrameworkElement)sender);
+        }
+
+        private static readonly Dictionary<string, Type> PageMap = new Dictionary<string, Type> {
+            { "layers", typeof(LayersPage) },
+            { "encounters", typeof(EncountersPage) },
+            { "playlists", typeof(PlaylistsPage) },
+            { "elements", typeof(ElementsPage) },
+            { "campaigns", typeof(EditCampaignsPage) },
+            { "playspaces", typeof(EditPlayspacesPage) }
+        };
     }
 }
