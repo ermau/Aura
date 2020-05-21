34f6ac9c5f534c73ab4a79c46542259fb5929625
Eric Maupin <me@ermau.com>
2020-05-20 23:47:30 -0400

    Add hue discovery service

---
+++ Aura.Hue/HueService.cs
@@ -0,0 +1,22 @@
+﻿using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Q42.HueApi;
+
+namespace Aura.Hue
+{
+	[Export (typeof(IService))]
+	public class HueService
+		: IDiscoverableService
+	{
+		public string DisplayName => "Philips Hue"; // TODO: Localize
+
+		public async Task<bool> DiscoverAsync ()
+		{
+			var scan = new LocalNetworkScanBridgeLocator ();
+			var bridges = await scan.LocateBridgesAsync (new CancellationTokenSource (5000).Token).ConfigureAwait (false);
+			return bridges.Any ();
+		}
+	}
+}
