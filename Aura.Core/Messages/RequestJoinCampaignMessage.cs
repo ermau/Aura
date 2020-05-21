854d54e9692a577f5c4c385d9b9a22ddedda0aab
Eric Maupin <me@ermau.com>
2020-05-20 23:52:23 -0400

    Join campaign flyout experience

---
+++ Aura.Core/Messages/RequestJoinCampaignMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aura.Service;
+
+namespace Aura.Messages
+{
+	internal class RequestJoinCampaignMessage
+	{
+		public RequestJoinCampaignMessage (RemoteCampaign campaign)
+		{
+			Campaign = campaign ?? throw new ArgumentNullException (nameof (campaign));
+		}
+
+		public RemoteCampaign Campaign
+		{
+			get;
+		}
+	}
+}
---
+++ Aura.Core/ViewModels/JoinCampaignRequestViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Aura.Messages;
+using Aura.Service;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace Aura.ViewModels
+{
+	internal class JoinCampaignRequestViewModel
+		: ViewModelBase
+	{
+		public JoinCampaignRequestViewModel (RemoteCampaign campaign)
+		{
+			Campaign = campaign ?? throw new ArgumentNullException (nameof (campaign));
+			JoinCommand = new RelayCommand (() => MessengerInstance.Send (new RequestJoinCampaignMessage (Campaign)));
+		}
+
+		public RemoteCampaign Campaign { get; }
+
+		public ICommand JoinCommand
+		{
+			get;
+		}
+
+		public string Message => $"Would you like to join {Campaign.Name}?";
+	}
+}
