99d9481dcc79c7165899a6bada4564c47a89c120
Eric Maupin <me@ermau.com>
2020-05-22 20:06:21 -0400

    Fixes to FlyoutService and missing refs

--- Aura/FlyoutService.cs
+++ Aura/FlyoutService.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿//
+// FlyoutService.cs
+//
+// Authors:
+//       Eric Maupin <me@ermau.com>
+//
+// Copyright (c) 2020 Eric Maupin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -65,9 +90,9 @@ namespace Aura
 				((FrameworkElement)oldFlyout.Content).DataContext = null;
 			}
 
+			CurrentFlyout = newFlyout;
 			if (newFlyout != null) {
 				newFlyout.Closed += OnFlyoutClosed;
-				CurrentFlyout = newFlyout;
 				CurrentFlyout.ShowAt (TargetElement, new FlyoutShowOptions {
 					Position = new Point (TargetElement.ActualWidth / 2, 40),
 					ShowMode = FlyoutShowMode.Transient
@@ -78,11 +103,8 @@ namespace Aura
 		private static void OnFlyoutClosed (object sender, object e)
 		{
 			var closed = (Flyout)sender;
-			closed.Closed -= OnFlyoutClosed;
-
-			if (Flyouts.TryPop (out Flyout flyout)) {
-				SwapFlyouts (closed, flyout);
-			}
+			Flyouts.TryPop (out Flyout flyout);
+			SwapFlyouts (closed, flyout);
 		}
 	}
 }
