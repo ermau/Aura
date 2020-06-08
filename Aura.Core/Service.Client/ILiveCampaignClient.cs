de8a4b661fd06205489f4a558e2f6ef1cb211c55
Eric Maupin <me@ermau.com>
2020-06-08 19:14:25 -0400

    Bring in service client

---
+++ Aura.Core/Service.Client/ILiveCampaignClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aura.Service.Client
+{
+	public interface ILiveCampaignClient
+	{
+		Task<RemoteCampaign> CreateCampaignAsync (string name, CancellationToken cancelToken);
+		Task<RemoteCampaign> GetCampaignDetailsAsync (string id, CancellationToken cancelToken);
+		Task<RemoteCampaign> GetCampaignDetailsAsync (Uri uri, CancellationToken cancelToken);
+	}
+}
