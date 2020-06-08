ab52fdce27350a962922b1b8bf3ac98c58f7a625
Eric Maupin <me@ermau.com>
2020-06-08 19:07:07 -0400

    PlaySpace manager

---
+++ Aura.Core/Data/PlaySpace.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura
+{
+	internal class PlaySpace
+		: NamedElement
+	{
+		
+	}
+}
---
+++ Aura.Core/PlaySpaceManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace Aura
+{
+	internal class PlaySpaceManager
+		: SingleSelectionManager<PlaySpace>
+	{
+		public PlaySpaceManager (ISyncService syncService)
+			: base (syncService)
+		{
+		}
+	}
+}
