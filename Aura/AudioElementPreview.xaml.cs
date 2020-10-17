7bfc13eee606d013a18ba76d128420cac5d9485e
Eric Maupin <me@ermau.com>
2020-10-17 02:01:21 -0400

    Prototype audio visualization

---
+++ Aura/AudioElementPreview.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Media;
+
+namespace Aura
+{
+	public sealed partial class AudioElementPreview : UserControl
+	{
+		public AudioElementPreview ()
+		{
+			InitializeComponent ();
+			SizeChanged += (o, e) => OnIntensityChanged ();
+		}
+
+		public static readonly DependencyProperty IntensityProperty = DependencyProperty.Register (nameof (Intensity), typeof (double), typeof (AudioElementPreview),
+			new PropertyMetadata (0, (dobj, e) => ((AudioElementPreview)dobj).OnIntensityChanged()));
+
+		public double Intensity
+		{
+			get => (double)GetValue (IntensityProperty);
+			set => SetValue (IntensityProperty, value);
+		}
+
+		private void OnIntensityChanged()
+		{
+			double i = Intensity;
+			//SetScale (this.originScale, (i > 0) ? 1 : 0);
+			SetScale (this.innerScale, i);
+			SetScale (this.outerScale, i);
+		}
+
+		private void SetScale (ScaleTransform transform, double scale)
+		{
+			transform.ScaleX = scale;
+			transform.ScaleY = scale;
+		}
+	}
+}
---
+++ Aura/PlaybackPreview.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Aura
+{
+	public sealed partial class PlaybackPreview : UserControl
+	{
+		public PlaybackPreview ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
