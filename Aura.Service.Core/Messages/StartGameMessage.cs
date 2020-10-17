8f66aaa2a7376026cc574b7ae001fb805f010bec
Eric Maupin <me@ermau.com>
2020-10-17 01:47:14 -0400

    fixup signalr

---
+++ Aura.Service.Core/Messages/StartGameMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.Service.Messages
+{
+	public class StartGameMessage
+	{
+		public string CampaignId
+		{
+			get;
+			set;
+		}
+
+		public string CampaignSecret
+		{
+			get;
+			set;
+		}
+	}
+}
