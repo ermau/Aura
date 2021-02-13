9f7ec2e849fad4956e106bc8e6762cf22cf6bcd3
Eric Maupin <me@ermau.com>
2021-02-13 16:22:32 -0500

    Add local storage service

---
+++ Aura.Core/ILocalStorageService.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Aura
+{
+	public interface ILocalStorageService
+	{
+		Task DeleteAsync (string id, string contentHash = null);
+
+		Task<Stream> TryGetStream (string id, string contentHash = null);
+
+		Task<Stream> GetWriteStreamAsync (string id, string contentHash = null);
+	}
+}
---
+++ Aura/Services/LocalStorageService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Aura.Services
+{
+	[Export (typeof (ILocalStorageService))]
+	internal class LocalStorageService
+		: ILocalStorageService
+	{
+		public async Task<Stream> TryGetStream (string id, string contentHash = null)
+		{
+			if (string.IsNullOrWhiteSpace (id))
+				throw new ArgumentException ($"'{nameof (id)}' cannot be null or whitespace", nameof (id));
+
+			try {
+				StorageFile file = await Storage.GetFileAsync (id).ConfigureAwait (false);
+				return await file.OpenStreamForReadAsync ().ConfigureAwait (false);
+			} catch (FileNotFoundException) {
+				return null;
+			}
+		}
+
+		public async Task<Stream> GetWriteStreamAsync (string id, string contentHash = null)
+		{
+			if (string.IsNullOrWhiteSpace (id))
+				throw new ArgumentException ($"'{nameof (id)}' cannot be null or whitespace", nameof (id));
+
+			// TODO: We can check for its existence against content hash later
+			StorageFile file = await Storage.CreateFileAsync (id, CreationCollisionOption.ReplaceExisting).ConfigureAwait (false);
+			return await file.OpenStreamForWriteAsync ().ConfigureAwait (false);
+		}
+
+		public async Task DeleteAsync (string id, string contentHash = null)
+		{
+			if (string.IsNullOrWhiteSpace (id))
+				throw new ArgumentException ($"'{nameof (id)}' cannot be null or whitespace", nameof (id));
+
+			try {
+				StorageFile file = await Storage.GetFileAsync (id).ConfigureAwait (false);
+				await file.DeleteAsync (StorageDeleteOption.PermanentDelete);
+			} catch (FileNotFoundException) {
+			}
+		}
+
+		private StorageFolder Storage
+		{
+			get { return ApplicationData.Current.LocalFolder; }
+		}
+	}
+}
